1e74d32a05f1e87c42069045543000fa4f8ddd21
kt-sec <stoke@fivemails.com>
2021-01-20 08:31:50 +0300

    Initial Release

---
+++ src/componets/ColorPicker/ColorInput.js
@@ -0,0 +1,55 @@
+import {useContext,memo} from 'react';
+//import InputColor from 'react-input-color';
+
+import ColorContext from '../../contexts/ColorContext';
+
+import {sendColor} from '../../services/websocket';
+
+import {hexToHSL} from '../../lib/hexToHSL';
+
+function ColorInput() {
+  const {user,color,setColor} = useContext(ColorContext);
+
+  const handleColorChange = (c) => {
+    const hslValue = hexToHSL(c.target.value);
+    setColor({
+      "username":user,
+      "hex":c.target.value,
+      "v":hslValue.s
+    });
+     sendColor({
+       "username":user,
+       "hex":c.target.value
+     });
+  }
+
+  return (
+    <input 
+      type="color"
+      className="colorPicker"
+      style={{
+        "height":"50px",
+        "width":"50px",
+        border:(color.v<60)?"2px solid white":"2px solid black"
+      }}
+      value={color.hex}
+      onChange={(c)=>{handleColorChange(c)}}
+      placement="center"
+    >
+    
+    </input>
+    // <InputColor 
+    //     className="colorPicker"
+    //     style={{
+    //       "height":"50px",
+    //       "width":"50px",
+    //       border:(color.v<60)?"2px solid white":"2px solid black"
+    //     }}
+    //     initialValue={color.hex}
+    //     onChange={(c)=>{handleColorChange(c)}}
+    //     placement="center"
+    //   />
+  )
+}
+
+export default memo(ColorInput);
